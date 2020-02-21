abd2acb47797b1d7d8cbcb0d2fb0bb739f52eb27
Mario Barrera <luismmario@gmail.com>
2020-02-21 12:54:05 -0700

    Exercise 14: Function and Test

---
+++ test/spec.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const app = require('../index');
+
+describe('App', function() {
+    it('app should return hello', function() {
+      assert.equal(app(), 'hello');
+    });
+  });
