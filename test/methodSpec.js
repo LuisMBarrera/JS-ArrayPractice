f24bfabcb6a98ad29c51d14cc782f403ef52a0ab
Mario Barrera <luismmario@gmail.com>
2020-02-24 15:22:42 -0700

    Initial Commit + Environment and Exercise 15

--- test/methodSpec.js
+++ test/methodSpec.js
@@ -1,26 +1,5 @@
 const assert = require('chai').assert;
 const flatten = require('../lib/Methods').flatten;
-const each = require('../lib/Methods').each;
-
-describe('Ex1', function() {
-    it('method should return a line for each object in the array showing name and age of each person', function() {
-        let testingResult = '\n1.- pedro is 19 years old\n2.- juan is 15 years old\n3.- pablo is 16 years old\n4.- pancho is 20 years old\n5.- topo is 18 years old\n';
-        let funcResult = '';
-        let people = [ 
-            {name: 'pedro', age: 19 },
-            {name: 'juan', age: 15 },
-            {name: 'pablo', age: 16 },
-            {name: 'pancho', age: 20 },
-            {name: 'topo', age: 18 }
-            ],
-            iterateThem = function (x, i) {
-                funcResult += (i + 1) + '.- ' + x.name +' is ' + x.age + ' years old\n';
-            };
-            
-        people.each(iterateThem);
-        assert.strictEqual(funcResult.trim(), testingResult.trim());
-    })
-})
 
 describe('Ex15', function() {
     it('method should return an array with no arrays inside', function() {
